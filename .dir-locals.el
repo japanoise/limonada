a3c9c4a0a3fcdabddc6281cd138a0c2e8fc6c88a
japanoise <japanoise@users.noreply.github.com>
2018-07-02 10:30:46 +0100

    Add dir-locals with clang include path
    Ugly as hell, but it makes flycheck work properly on my home computer.
    Will need some additional modifications to work properly at work.

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((c-mode . ((flycheck-clang-include-path . ("/usr/include/gtk-2.0" "/usr/lib/x86_64-linux-gnu/gtk-2.0/include"
+                                            "/usr/include/gio-unix-2.0/" "/usr/include/cairo"
+                                            "/usr/include/pango-1.0" "/usr/include/atk-1.0"
+                                            "/usr/include/cairo" "/usr/include/pixman-1"
+                                            "/usr/include/libpng12" "/usr/include/gdk-pixbuf-2.0"
+                                            "/usr/include/libpng12" "/usr/include/pango-1.0"
+                                            "/usr/include/harfbuzz" "/usr/include/pango-1.0"
+                                            "/usr/include/glib-2.0" "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+                                            "/usr/include/freetype2")))))
