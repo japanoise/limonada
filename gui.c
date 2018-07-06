d0b51654b9e645af6750a6ffadcd202f64d5c908
japanoise <japanoise@tfwno.gf>
2018-07-06 12:02:54 +0100

    Use unsigned chars in drawtext
    This allows us to use the high-ascii included in cp437

--- gui.c
+++ gui.c
@@ -26,7 +26,7 @@ void drawText(SDL_Renderer * rend, char *text, SDL_Texture * font, int x, int y)
 	SDL_Rect srcrect = { 0, 0, LETWIDTH, LETHEIGHT };
 	SDL_Rect destrect = { x, y, LETWIDTH, LETHEIGHT };
 	for (; *text != '\0'; text++) {
-		char c = *text;
+		unsigned char c = *text;
 		srcrect.x = (c & 0x0F) * LETWIDTH;
 		srcrect.y = (c >> 4) * LETHEIGHT;
 		SDL_RenderCopy(rend, font, &srcrect, &destrect);
